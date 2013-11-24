7217c1722a2423c72bbffdae079bdbf36191778c
cilop <robertoatc02@gmail.com>
2013-11-24 14:19:28 -0800

    simpleAdding.js
    Have the function SimpleAdding(num) add up all the numbers from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000.

---
+++ simpleAdding.js
@@ -0,0 +1,12 @@
+function SimpleAdding(num) { 
+//return the sum of every number up to and including 'num'
+  var aux = 0;
+  for (var i =1;i<=num;i++)
+  {
+    aux = aux + i;
+  }
+  
+  
+  return aux; 
+         
+}
