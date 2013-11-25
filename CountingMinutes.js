0e08d3f135ed0ff678a79bcf6de3afcc95fbe85f
cilop <robertoatc02@gmail.com>
2013-11-25 17:10:28 -0600

    count difference minutes between 2 hour strings

---
+++ CountingMinutes.js
@@ -0,0 +1,38 @@
+/*Using the JavaScript language, have the function CountingMinutesI(str) take the str 
+parameter being passed which will be two times (each properly formatted with a colon 
+and am or pm) separated by a hyphen and return the total number of minutes between 
+the two times. The time will be in a 12 hour clock format. For example: if str is 
+9:00am-10:00am then the output should be 60. If str is 1:00pm-11:00am the output 
+should be 1320. */
+
+var CountingMinutesI  = function (str)
+
+{
+    var times = str.split('-');
+    var min1;
+    var hr1;
+    
+    var min2;
+    var hr2;
+    
+    var ans;
+    
+    hr1 = parseInt(times[0].split(":")[0]);
+    min1 = parseInt(times[0].split(":")[1].split(/[a-z]/)[0]);
+        
+    hr2 = parseInt(times[1].split(":")[0]);
+    min2 = parseInt(times[1].split(":")[1].split(/[a-z]/)[0]);
+    
+    if (times[0].search(/[a]/) == times[1].search(/[a]/) || times[0].search(/[p]/) == times[1].search(/[p]/))
+    {
+        
+        ans = Math.abs(hr1 - hr2)*60 + (min2 - min1);
+                
+    }
+    else
+    {
+        
+        ans = (12 - (hr1 - hr2)%12)*60 + (min2 - min1);
+    }
+    return ans;
+}
