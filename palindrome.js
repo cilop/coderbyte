d3319a242d26512d7c52f0bb40735c09cb828785
cilop <robertoatc02@gmail.com>
2013-11-29 17:10:12 -0600

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,23 @@
+//Have the function Palindrome(str) take the str parameter being passed and
+//return the string true if the parameter is a palindrome, (the string is the
+//same forward as it is backward) otherwise return the string false. For example:
+//"racecar" is also "racecar" backwards. Punctuation and numbers will not be part
+//of the string
+
 function Palindrome(str) { 
+
+  str = str.split(' ').join('');
   
-  var str2 = str.replace(/[ ]/gi,'').split('').reverse().join('');
-  str =str.replace(/[ ]/gi,'');
-  if (str == str2) {return true}
-  else {return false}     
-}
-   
-// keep this function call here 
-// to see how to enter arguments in JavaScript scroll down
-Palindrome(readline());   
+  for (var i = 0;i<str.length/2;i++)
+  {
+    if (str[i] != str[str.length  -1- i])
+    {
+      return false
+        }
+  }
+  
+  return true
+ 
+  
+         
+} 
