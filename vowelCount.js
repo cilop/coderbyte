ea8cd02ead52461bb9c9f5595f4eeccf7493b7e7
cilop <robertoatc02@gmail.com>
2013-11-29 17:07:25 -0600

    Update vowelCount.js

--- vowelCount.js
+++ vowelCount.js
@@ -2,15 +2,7 @@
 
 function VowelCount(str) { 
 
-  var re = /[a|e|i|o|u]/g;
-  var m;
-  var i=-1;
-
-  do {
-  m = re.exec(str)
-  i ++;
-  } while (m);
   
-  return i; 
+  return str.match(/[aeiou]+/ig).join('').split('').length; 
          
 }
