4644ab05ca1f50cab30616f718fa8a50d8604b71
cilop <robertoatc02@gmail.com>
2013-11-29 17:15:31 -0600

    Update CountingMinutes.js

--- CountingMinutes.js
+++ CountingMinutes.js
@@ -36,3 +36,38 @@ var CountingMinutesI  = function (str)
     }
     return ans;
 }
+
+// TRY 2:
+
+function CountingMinutesI_try2(str) { 
+
+  
+  var times = str.split('-');
+  var auxarr,h1,m1,h2,m2,ans;
+  
+  auxarr = times[0].match(/[^a-z]+/ig)[0].split(':');
+  h1 = parseInt(auxarr[0]);
+  m1 = parseInt(auxarr[1]);
+  
+  auxarr = times[1].match(/[^a-z]+/ig)[0].split(':');
+  h2 = parseInt(auxarr[0]);
+  m2 = parseInt(auxarr[1]);
+  
+  if (times[0][times[0].length-2] == times[1][times[1].length-2])
+  {
+    //ans = ((h2-h1)*60 + (m2-m1))%1440;
+    if (h2 > h1)
+     ans =  ((h2-h1)*60 + (m2-m1))%1440;
+    
+    else
+      ans = 1440 +  ((h2-h1)*60 + (m2-m1))%1440;
+  }
+  else
+  {
+    ans =  ((12 + (h2-h1)%12)*60 + (m2-m1));
+  }
+  
+  
+    return ans;
+         
+}
